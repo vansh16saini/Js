d5d555b2abdffaf2d4e57f4b5029b3409283dd78
Vansh Saini <124070153+vansh16saini@users.noreply.github.com>
2024-10-01 20:58:25 +0530

    Datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") we are using nodejs not browser
+console.log(3+4) 
+console.log("Vansh")//readability is a proiority and should be high
+
+let name = "Vansh"
+let age = 19
+let isloggedIn = false
+// number -> 2 to power 53
+//bigInt a lot big number
+// string ""
+// boolean true/false
+//null
+//undefined -> the value is not assigned yet
+// symbol -> for unique
+
+//NULL is a object WHILE undefined is undefined
+//object
+console.log(typeof age)
