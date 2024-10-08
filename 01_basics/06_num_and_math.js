3d15d248f2a80850288a8c8a3ae46d370c69d84c
Vansh Saini <124070153+vansh16saini@users.noreply.github.com>
2024-10-08 09:59:51 +0530

    math

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -1,4 +1,4 @@
-const score = 500
+/*const score = 500
 console.log(score)
 
 const balance = new Number(100)
@@ -13,3 +13,26 @@ console.log(other_Number.toPrecision(3))
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN'))
+*/
+//+++++++++++++++++ Maths+++++++++++++++
+/*
+console.log(Math);
+console.log(Math.abs(-23));
+console.log(Math.round(4.64));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.7));
+console.log(Math.abs(-23));
+console.log(Math.min(2,3,4,2,1,4));
+console.log(Math.max(2,3,4,2,1,4));
+*/
+// maximum use 
+console.log(Math.random());
+// values will shift to 1
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1 )))
