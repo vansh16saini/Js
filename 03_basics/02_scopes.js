4f4c4f8a161cd28d09a91eb576ecde4895b12490
Vansh Saini <124070153+vansh16saini@users.noreply.github.com>
2024-10-17 19:58:31 +0530

    scope level

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,9 +7,53 @@ if (true){
     let a = 2
 const b = 20
 var c = 30
-console.log("Inner:",a);
+// console.log("Inner:",a);
 
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// NESTED SCOPE
+
+// function one(){
+//     const usesrname = "Vansh"
+
+//     function two(){
+//         const website = 'youtube'
+//         console.log(usesrname);
+//     }
+//     // console.log(website);  we had an error here as website's scope was in function named 2 only
+//     two()
+// }
+
+// one()
+
+// if-else is also a type of scope
+// if(true){
+//     const usesrname = "Vansh"
+//     if(usesrname=== "Vansh"){
+//         const website = "youtube"
+//         console.log(usesrname + website); 
+//     }
+//     // console.log(website);  here error is that website is not in scope
+// }
+// Well for a reference we can say a junior can ask for help from senior but not vise-versa
+// console.log(usesrname);
+
+
+// ***********************Interesting*************************
+addone(5)
+function addone(num){
+    return num + 1;
+}
+
+
+// well both of them are functions but the function below is also a variable
+// and variables are very powerful and we can hold anything in them
+addtwo(5)
+// we have an error as we shifted addtwo()  to above
+const addtwo = function(num){
+    return num + 2;
+}
+// this is called hosting and we will learn them about later on
