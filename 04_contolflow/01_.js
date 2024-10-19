fcee248d8eaa8d53f23cc7926f71dfdfdfe8f7c4
Vansh Saini <124070153+vansh16saini@users.noreply.github.com>
2024-10-19 21:46:41 +0530

    loops

--- 04_contolflow/01_.js
+++ 04_contolflow/01_.js
@@ -12,10 +12,26 @@ if(false){
 //comparison operators
 // <, >, <=, >=, ==, !=, === it even checks the datatype as well, !== this even checks -ve value
 // here single = checks operator assignment
-const temperature = 41
-if (temperature < 50){
+// const temperature = 41
+// if (temperature < 50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("greater than 50");
+// }
+// const score = 100
+// if (score > 100){
+//     const power = 'fly'
+//     console.log(`power greater than ${power}`);
+// } 
+
+const balance = 10000
+if (balance > 100)//this is implicit scope  
+    console.log("less than 100");
+    // implicit scope is immature 
+else if (balance > 50)
     console.log("less than 50");
-}
-else{
-    console.log("greater than 50");
-}
+    
+else 
+    console.log("Nothing");
+    
