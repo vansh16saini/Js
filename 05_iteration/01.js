174616b2415607211a4ffaad6dacdb7b5bc0adaf
Vansh Saini <124070153+vansh16saini@users.noreply.github.com>
2024-10-25 21:44:51 +0530

    for loop

---
+++ 05_iteration/01.js
@@ -0,0 +1,19 @@
+// for
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i );   
+//     if(i == 0)
+//         console.log("0 is developed by india");
+//     console.log(i);    
+// }
+// let array = [1,2,4,5,6]
+// for (let i = 0; i < array.length; i++) {
+//     console.log(`outer loop value: ${i}`)
+//     for (let j = 0; j < array.length; j++) {
+//         // console.log(`inner loop value: ${j}`)
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+// }
+
+
+//  
