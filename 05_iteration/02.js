87ff54a76015d432f0bf5129a8f043859d478479
Vansh Saini <124070153+vansh16saini@users.noreply.github.com>
2024-10-26 17:53:27 +0530

    while do while

---
+++ 05_iteration/02.js
@@ -0,0 +1,23 @@
+// while (2==4) {
+//     console.log("What's UP")
+// }
+// let index = 0
+// while(index == 0){
+//     console.log(`value is ${index}`)
+//     index = index +3
+// }
+
+// let myArray = ['random','names','hello','']
+// let arr = 0
+// while(arr < myArray.length){
+//     console.log(`value of ${myArray[arr]}`)
+//     arr = arr + 1
+// } 
+
+// dowhile
+
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score = score + 1
+} while (score<=10);
