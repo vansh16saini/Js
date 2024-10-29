c6907de91cd8106fcaf462117b0f84cfdd319b33
Vansh Saini <124070153+vansh16saini@users.noreply.github.com>
2024-10-29 10:14:04 +0530

    forEach

---
+++ 05_iteration/05.js
@@ -0,0 +1,30 @@
+// for each
+const coding = ['js','ruby','cpp','html','css','rr']
+
+// for each is a higher order
+
+// coding.forEach(function (item) {
+//     console.log(item)
+// })
+
+// coding.forEach( () => {
+//     console.log()
+// })
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr)
+// })
+
+const mycoding =[
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },{
+        languageName: "java",
+        languageFileName: "java"
+    },{
+        languageName: "C++",
+        languageFileName: "cpp"
+    },
+]
+mycoding.forEach(()=>{ })
